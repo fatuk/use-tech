e445c829f351c782eb3e148fb555ce169dfd5b5d
fatuk <fatukk@gmail.com>
2014-10-01 17:39:13 +0400

    Add clients slider

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -6,7 +6,8 @@ module.exports = {
         files: {
             'vendors/plugins.min.js': [
                 'bower_components/jquery/dist/jquery.js',
-                'bower_components/fractionslider/jquery.fractionslider.min.js'
+                'bower_components/fractionslider/jquery.fractionslider.min.js',
+                'bower_components/jQuery-Open-Carousel/jquery.openCarousel.js'
             ]
         }
     }
