df59cc35b8df2cebb1fb8f6b0b19b46530e241e6
fatuk <fatukk@gmail.com>
2014-10-03 11:31:46 +0400

    Fix site header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 $(function() {
-	$('.slider').fractionSlider({
-		timeout: 2000,
-		pager: true,
-		responsive: false
-	});
+    $('.slider').fractionSlider({
+        timeout: 2000,
+        pager: true,
+        responsive: false
+    });
+
+    var isMenuOpened = false;
+
+    $('.js-menuBtn').on('click', function(event) {
+        if (!isMenuOpened) {
+            $('.js-menu').slideDown('fast');
+            isMenuOpened = true;
+        } else {
+            $('.js-menu').slideUp('fast');
+            isMenuOpened = false;
+        }
+    });
+
+    $(window).on('resize', function() {
+        var $menu = $('.js-menu');
+        if ($(window).width() >= 1200) {
+            $menu.css({
+                'display': 'inline-block'
+            });
+        } else {
+            $menu.css({
+                'display': 'none'
+            });
+        }
+    });
 });
